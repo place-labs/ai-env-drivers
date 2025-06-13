42e351114cc735a88c0dbc445ea7fc8a2fd530ed
Stephen von Takach <steve@place.technology>
2025-06-13 15:44:47 +1000

    fix setup

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@
 TARGET_DIR="drivers"
 REPO_URL="https://github.com/PlaceOS/drivers.git"
 
+git submodule update --init --recursive
+
 if [ -d "$TARGET_DIR/.git" ]; then
     echo "Repository already exists. Pulling latest changes..."
     cd "$TARGET_DIR" && git pull --ff-only
@@ -12,7 +14,7 @@ else
     git clone --depth 1 "$REPO_URL" "$TARGET_DIR"
 fi
 
-git submodule update --init --recursive
+shards install
 
 echo '░░░ Setup complete...'
 
